e39505cfa7381ae9e34ed15944ec6bf9acb47718
cassiogroh <cassio_groh@hotmail.com>
2020-08-14 17:00:28 -0300

    Creating repo

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,21 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Landing from './src/pages/Landing';
+import { AppLoading } from 'expo';
+
+import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo';
+import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
+
+import AppStack from './src/routes/AppStack';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  let [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <>
+        <AppStack />
+        <StatusBar style="light" />
+      </>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// npm install expo --global      (expo init mobile -> blank typescript )
+
+// Fonts for expo from google fonts
+// expo install expo-font @expo-google-fonts/archivo @expo-google-fonts/poppins
+
+// React navigation (https://reactnavigation.org/   ->  read docs)
+// npm install @react-navigation/native
+// expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view
+
+// Stack and Tabs navigation
+// npm install @react-navigation/stack
+// npm install @react-navigation/bottom-tabs
+
+// Conexão com a API
+// npm install axios
+
+// Storage on phone
+// expo install @react-native-community/async-storage
+
+// Upgrades
+// npm install @react-native-community/picker --save
+// Para fazer caixas de seleção na aba de listagem de professores
