082344c9e99aedce517853a0686f6ed768d0f1f6
cassiogroh <cassio_groh@hotmail.com>
2020-08-14 17:02:34 -0300

    Creating repo

---
+++ server/src/server.ts
@@ -0,0 +1,21 @@
+// npm install typescrypt -D
+// npx tsc --init
+// npm install ts-nose-dev -D    (como nodemon, para atualizar sem reiniciar o servidor)
+// npm install express
+// npm install @types/express -D
+// npm install knex sqlite3
+// npm install cors
+// npm install @types/cors -D
+
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+
+app.listen(3333);
