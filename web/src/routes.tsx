ef7760e606e353e2a16e22dde2fb2ba746c171d9
cassiogroh <cassio_groh@hotmail.com>
2020-08-14 16:58:50 -0300

    Creating repo

---
+++ web/src/routes.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Landing from './pages/Landing';
+import TeacherList from './pages/TeacherList';
+import TeacherForm from './pages/TeacherForm';
+
+// npm install react-router-dom
+// npm install @types/react-router-dom -D
+
+const Routes = () => {
+    return (
+        <BrowserRouter>
+            <Route path='/' exact component={Landing}/>
+            <Route path='/study' component={TeacherList}/>
+            <Route path='/give-class' component={TeacherForm}/>
+        </BrowserRouter>
+    )
+}
+
+export default Routes;
