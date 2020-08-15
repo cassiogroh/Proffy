6efd2205992ef8286d6f1d7e2a8dccf90e87fb95
cassiogroh <cassio_groh@hotmail.com>
2020-08-15 18:22:57 -0300

    cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,6 +13,12 @@ import routes from './routes';
 
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://proffy-api-server.herokuapp.com/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(routes);
