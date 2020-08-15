f1334423ad9227803d86ee04221524a32552bf84
cassiogroh <cassio_groh@hotmail.com>
2020-08-15 18:45:50 -0300

    connected API

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://proffy-api-server.herokuapp.com/',
+    baseURL: 'http://localhost:3000/',
 })
+
+// https://proffy-api-server.herokuapp.com/
+// http://localhost:3000/
 
 export default api;
