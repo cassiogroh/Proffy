e1fa2c7144396206940ce08af4fbb0d2e70b5c11
cassiogroh <cassio_groh@hotmail.com>
2020-08-15 18:29:30 -0300

    Cors allowed

--- server/src/server.ts
+++ server/src/server.ts
@@ -14,7 +14,7 @@ import routes from './routes';
 const app = express();
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://proffy-api-server.herokuapp.com/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://localhost:3001"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
