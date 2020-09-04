939957b63e76c553a38f14711289cd3675f7761c
cassiogroh <cassio_groh@hotmail.com>
2020-09-04 01:01:00 -0300

    Changed DB to Postgres

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -26,17 +26,17 @@ export default class ClassesController {
     const timeInMinutes = convertHourToMinutes(time);
 
     const classes = await db('classes')
-      .whereExists(function () {
+      .whereExists(function() {
         this.select('class_schedule.*')
           .from('class_schedule')
-          .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
-          .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
-          .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
-          .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes])
+          .whereRaw('class_schedule.class_id = classes.id')
+          .whereRaw('class_schedule.week_day = ??', [Number(week_day)])
+          .whereRaw('class_schedule.from <= ??', [timeInMinutes])
+          .whereRaw('class_schedule.to > ??', [timeInMinutes])
       })
       .where('classes.subject', '=', subject)
       .join('users', 'classes.user_id', '=', 'users.id')
-      .select(['classes.*', 'users.*']);
+      .select(['classes.*', 'users.*'])
 
     return res.json(classes)
   }
@@ -55,20 +55,20 @@ export default class ClassesController {
     const trx = await db.transaction();
 
     try {
-      const insertedUsersIds = await trx('users').insert({
-        name,
-        avatar,
-        whatsapp,
-        bio
-      });
+    const insertedUsersIds = await trx('users').insert({
+      name,
+      avatar,
+      whatsapp,
+      bio
+    }).returning('id');
 
-      const user_id = insertedUsersIds[0];
+    const user_id = insertedUsersIds[0];
 
-      const insertedClassesIds = await trx('classes').insert({
-        subject,
-        cost,
-        user_id
-      });
+    const insertedClassesIds = await trx('classes').insert({
+      subject,
+      cost,
+      user_id
+    }).returning('id');
 
       const class_id = insertedClassesIds[0];
 
